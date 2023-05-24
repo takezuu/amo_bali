5194a33dbfdf4222cc0a1093aa13ac9ddbbc89a0
nik <you@example.com>
2023-05-24 12:05:02 +0200

    Added stop list to changed status

--- DataFunc.py
+++ DataFunc.py
@@ -627,15 +627,14 @@ def get_lost_stage(data) -> list:
                         lost_stage_list.append([(lead['value_before'][0]['lead_status']['pipeline_id'],
                              lead['value_before'][0]['lead_status']['id']), lead['entity_id']])
 
-
-
         final_lost_stage = []
         for lead in lost_stage_list:
             pipeline = lead[0][0]
-            pipeline = statuses_dict[f'{pipeline}']
-            for stage in pipeline:
-                if stage[0] == lead[0][1]:
-                    final_lost_stage.append((stage[1], lead[1]))
+            if pipeline not in [5298025, 6229330, 5297782, 6245694, 6181218, 6437666, 6693290, 62293]:
+                pipeline = statuses_dict[f'{pipeline}']
+                for stage in pipeline:
+                    if stage[0] == lead[0][1]:
+                        final_lost_stage.append((stage[1], lead[1]))
 
         return final_lost_stage
 
@@ -663,10 +662,11 @@ def get_lost_stage_update(data):
         final_lost_stage = []
         for lead in lost_stage_list:
             pipeline = lead[0][0]
-            pipeline = statuses_dict[f'{pipeline}']
-            for stage in pipeline:
-                if stage[0] == lead[0][1]:
-                    final_lost_stage.append((stage[1], lead[1]))
+            if pipeline not in [5298025, 6229330, 5297782, 6245694, 6181218, 6437666, 6693290, 62293]:
+                pipeline = statuses_dict[f'{pipeline}']
+                for stage in pipeline:
+                    if stage[0] == lead[0][1]:
+                        final_lost_stage.append((stage[1], lead[1]))
 
         return final_lost_stage
 
